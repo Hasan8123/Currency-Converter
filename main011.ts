4840dcaeb31419a70e15dd6689d894ee45f50bdb
Hasan Sheikh <hasansheikh8123@gmail.com>
2023-09-15 21:10:57 +0500

    Initial commit

---
+++ main011.ts
@@ -0,0 +1,67 @@
+
+
+
+
+
+import inquirer from "inquirer"
+
+let Convertion= {
+    "PKR": {
+        "USD": 0.00335,
+        "EUR": 0.00315,
+        "GBP": 0.00270,
+        "PKR": 1
+    },
+    "USD": {
+        "USD": 1,
+        "EUR": 0.94,
+        "GBP": 0.80,
+        "PKR": 300
+    },
+    "EUR": {
+        "USD": 1.06,
+        "EUR": 1,
+        "GBP": 0.85,
+        "PKR": 317.18
+    },
+    "GBP": {
+        "USD": 1.24,
+        "EUR": 1.16,
+        "GBP": 1,
+        "PKR": 369.94
+    },
+} 
+
+const answer: {
+    from: "PKR"| "USD"| "EUR"| "GBP", 
+    to: "PKR"| "USD"| "EUR"| "GBP",
+    amount: number
+} = await inquirer.prompt([
+    {
+        type: "list",
+        name: "from",
+        choices: ["PKR", "USD", "EUR", "GBP"],
+        message: "Select Your Currency: "
+    },
+    {
+        type: "list",
+        name: "to",
+        choices: ["PKR", "USD", "EUR", "GBP"],
+        message: "Select Your Convertion Currency: "
+    },
+    {
+        type: "number",
+        name: "amount",
+        message: "Enter Your Convertion Amount: "
+    }
+]);
+
+const {from, to, amount} = answer;
+
+if(from && to && amount){
+    let result= Convertion [from][to] *amount;
+    console.log(` Your Convertion From ${from} To ${to} Is ${result}`)
+}
+else{
+    console.log("Invalid Inputs")
+}
